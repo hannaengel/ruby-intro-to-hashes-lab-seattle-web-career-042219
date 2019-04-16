ed27ef1aba0757cf0149e75aa12b1efc640d5b5d
Hanna Engel <hengel@alumni.berklee.edu>
2019-04-16 22:36:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,14 +12,11 @@ end
 
 def monopoly_with_second_tier
   monopoly = {
-    :railroads => {}
+    :railroads => {:pieces => 4}
   }
 end
 
 def monopoly_with_third_tier
-  monopoly = {
-    :railroads => {:pieces => 4}
-  }
 
 
 end
